8b4d734833f0cb9e1cd6cb9d42a5715fda2a87a8
gerald prambudi <geraldprambudi94@gmail.com>
2018-08-12 08:40:28 +0700

    tambah php

--- FbChatMock.php
+++ FbChatMock.php
@@ -1,61 +1,63 @@
-class FbChatMock {
-    private $dbConnection;
+<?php
+    class FbChatMock {
+        private $dbConnection;
 
-    private $_dbHost = 'localhost';
-    private $_dbUsername = 'root';
-    private $_dbPassword = '';
-    private $_databaseName = 'facebook_messenger';
+        private $_dbHost = 'localhost';
+        private $_dbUsername = 'root';
+        private $_dbPassword = '';
+        private $_databaseName = 'facebook_messenger';
 
-    public function __construct() {
-        $this->dbConnection = new mysqli($this->$_dbHost, $this->$_dbUsername, $this->$_dbPassword, $this->$_databaseName);
+        public function __construct() {
+            $this->dbConnection = new mysqli($this->$_dbHost, $this->$_dbUsername, $this->$_dbPassword, $this->$_databaseName);
 
-        if($this->dbConnection->connect_error) {
-            die('Connection error');
+            if($this->dbConnection->connect_error) {
+                die('Connection error');
+            }
         }
-    }
 
-    public function getMessages() {
-        $messages = array();
-        $query = "
-            SELECT
-                'chat'.'message',
-                'chat'.'sent_on',
-                'users'.'id',
-                'users'.'username',
-            FROM 'users'
-            JOIN 'chat'
-            ON 'chat'.'user_id' = 'users'.'id'
-            ORDER BY 'sent_on'";
+        public function getMessages() {
+            $messages = array();
+            $query = "
+                SELECT
+                    'chat'.'message',
+                    'chat'.'sent_on',
+                    'users'.'id',
+                    'users'.'username',
+                FROM 'users'
+                JOIN 'chat'
+                ON 'chat'.'user_id' = 'users'.'id'
+                ORDER BY 'sent_on'";
 
-            
-            $resultObj = $this->dbConnection->query($query);
+                
+                $resultObj = $this->dbConnection->query($query);
 
-            while($row = $resultObj->fetch_assoc()) {
-                $messages[] = $row;
+                while($row = $resultObj->fetch_assoc()) {
+                    $messages[] = $row;
+                }
+
+                return $messages;
+        }
+
+
+        public function addMessage($userId, $message) {
+            $addResult = false;
+
+            $cUserId = (int) $userId;
+
+            $cMessage = $this->dbConnection->real_escape_string($message);
+
+            $query = "
+                INSERT INTO 'chat'('user_id', 'message', 'sent_on')
+                VALUES ({$cUserId}, '{$cMessage}', UNIX_TIMESTAMP())";
+            $result = $this->dbConnection->query($query);
+
+            if($result !== false) {
+                $addResult = $this->dbConnection->insert_id;
+            } else {
+                echo $this->dbConnection->error;
             }
 
-            return $messages;
-    }
-
-
-    public function addMessage($userId, $message) {
-        $addResult = false;
-
-        $cUserId = (int) $userId;
-
-        $cMessage = $this->dbConnection->real_escape_string($message);
-
-        $query = "
-            INSERT INTO 'chat'('user_id', 'message', 'sent_on')
-            VALUES ({$cUserId}, '{$cMessage}', UNIX_TIMESTAMP())";
-        $result = $this->dbConnection->query($query);
-
-        if($result !== false) {
-            $addResult = $this->dbConnection->insert_id;
-        } else {
-            echo $this->dbConnection->error;
+            return $addResult;
         }
-
-        return $addResult;
     }
-}
+?>
